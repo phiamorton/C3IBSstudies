7fa83574c4532305a973ee96737087f8c808dceb
Phia Morton <phia@DN51s51g.SUNet>
2025-04-18 10:56:08 -0700

    update filename

--- c3ibssim.bat
+++ c3ibssim.bat
@@ -1,4 +1,4 @@
 python make_lat_4_6.py --lat_name=MYBL_4_6
 elegant single_bunch_ibs_4_6.ele
-sddscombine IBSoutput_4_6* -merge -pipe=out | sddssort -pipe=in -column=s runAll_4_6.ibs
+sddscombine IBSOUTPUT_4_6* -merge -pipe=out | sddssort -pipe=in -column=s runAll_4_6.ibs
 sddsplot -column=s,dIBSRatey runAll_4_6.ibs -separate
