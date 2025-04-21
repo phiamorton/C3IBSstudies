556b919baff42a861a3173c2173ffd2d879fe513
Phia Morton <phia@dhcp-visitor-enr-114-243.slac.stanford.edu>
2025-04-21 09:37:35 -0700

    rm output files

--- c3ibssim.bat
+++ c3ibssim.bat
@@ -1,4 +1,5 @@
 python make_lat_4_6.py --lat_name=MYBL_4_6
 elegant single_bunch_ibs_4_6.ele
 sddscombine IBSOUTPUT_4_6* -merge -pipe=out | sddssort -pipe=in -column=s runAll_4_6.ibs
+rm IBSOUTPUT_4_6*
 sddsplot -column=s,dIBSRatey runAll_4_6.ibs -separate
